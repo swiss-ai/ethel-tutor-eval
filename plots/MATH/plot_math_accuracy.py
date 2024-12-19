5703fa96ffa301c45daf0a9bcd118ab1c41199b3
Michele Smaldone <michele.smaldone@usi.ch>
2024-12-19 12:17:46 +0100

    MATH accuracies.

--- plots/MATH/plot_math_accuracy.py
+++ plots/MATH/plot_math_accuracy.py
@@ -11,7 +11,7 @@ def plot_model_accuracies(datasets):
                 - 'n_shots' (str): The number of shots.
                 - 'accuracy' (float): The accuracy of the model.
     """
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(5, 3))
 
     for model_name, data in datasets:
         # Extract n_shots and accuracies from the data
@@ -24,8 +24,8 @@ def plot_model_accuracies(datasets):
     # Add labels and title
     plt.xlabel('Number of Shots')
     plt.ylabel('Accuracy')
-    plt.title('MATH dataset Comparison')
-    plt.xticks(rotation=45, ha='right')  # Rotate x-axis labels for better readability
+    plt.title('MGSM language accuracy Comparison')
+    plt.xticks()  # Rotate x-axis labels for better readability
     plt.grid(True, linestyle='--', alpha=0.7)
     plt.legend()
 
@@ -34,29 +34,24 @@ def plot_model_accuracies(datasets):
     plt.show()
 
 # Example usage
-data1 = [
-    {"n_shots": "0", "accuracy": 0.85},
-    {"n_shots": "2", "accuracy": 0.38},
-    {"n_shots": "4", "accuracy": 0.22},
-    {"n_shots": "8", "accuracy": 0.58}
+data1 = [ #llama-3.1 french
+    {"n_shots": "0", "accuracy": 0.715},
+    {"n_shots": "2", "accuracy": 0.824},
+    {"n_shots": "4", "accuracy": 0.848},
+    {"n_shots": "6", "accuracy": 0.863},
+    {"n_shots": "8", "accuracy": 0.836},
 ]
-data2 = [
-    {"n_shots": "0", "accuracy": 0.15},
-    {"n_shots": "2", "accuracy": 0.68},
-    {"n_shots": "4", "accuracy": 0.02},
-    {"n_shots": "8", "accuracy": 0.88}
-]
-data3 = [
-    {"n_shots": "0", "accuracy": 0.35},
-    {"n_shots": "2", "accuracy": 0.78},
-    {"n_shots": "4", "accuracy": 0.92},
-    {"n_shots": "8", "accuracy": 0.88}
+data2 = [ #llama-3.1 german
+    {"n_shots": "0", "accuracy": 0.592},
+    {"n_shots": "2", "accuracy": 0.80},
+    {"n_shots": "4", "accuracy": 0.811},
+    {"n_shots": "6", "accuracy": 0.828},
+    {"n_shots": "8", "accuracy": 0.828}
 ]
 
 datasets = [
-    ("Model A", data1),
-    ("Model B", data2),
-    ("Model C", data3)
+    ("Llama-3.1 french MGSM", data1),
+    ("Llama-3.1 german MGSM", data2)
 ]
 
 plot_model_accuracies(datasets)
